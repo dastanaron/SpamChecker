b72db8f5abc1ea654be3d3d29ffa62e5399e5584
dastanaron <flow199@yandex.ru>
2017-10-17 13:04:49 +0300

    bug fixed

---
+++ IPList.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace dastanaron\spamipcheck;
+
+class IPList {
+    
+    public $blacklist = array();
+        
+    
+    public function __construct()
+    {
+        $list = file_get_contents( __DIR__.'/blacklist.txt');
+        $this->blacklist($list);
+    }
+    
+    public static function getInstance()
+    {
+        return new IPList();
+    }
+    
+    
+    private function blacklist($list)
+    {
+        if($this->BuildArrayList($list)) {
+            return true; 
+        }
+        else {
+            return false;
+        }
+    }
+    
+    private function BuildArrayList($list)
+    {
+        $this->blacklist = explode(PHP_EOL, $list);
+        
+        $this->blacklist = $this->strip_space($this->blacklist);
+        
+        if(is_array($this->blacklist)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        
+    }
+    
+    private function strip_space($data)
+    {
+        
+        if(is_array($data)) {
+            $new_array = [];
+            foreach ($data as $elem) {
+                if(!empty($elem)) {
+                    $new_array[] = trim($elem);
+                }
+            }
+            return $new_array;
+        }
+        
+        return trim($new_array);
+        
+    }
+    
+}
