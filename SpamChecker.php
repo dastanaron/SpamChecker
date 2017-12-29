8c0667996966d01b1342529d6af42173e81d6a0a
dastanaron <flow199@yandex.ru>
2017-12-29 17:42:09 +0300

    Release 2.0

---
+++ SpamChecker.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace dastanaron;
+
+class SpamChecker {
+
+    private $blacklist = [];
+
+    public function __construct($path_to_list, $limit = 0) {
+
+        $this->blacklist = file($path_to_list, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
+
+        if ($limit > 0) {
+            $this->blacklist = array_slice($this->blacklist, 0, $limit);
+        }        
+
+    }
+
+    public function check($host) {
+
+        $result = [
+            "host" => $host,
+            "blacklists" => [],
+        ];
+
+        $host_revert = join(".", array_reverse(explode(".", $host)));
+
+        foreach ($this->blacklist as $dns) {
+
+            $response = @dns_get_record($host_revert . '.' . $dns, DNS_TXT);
+
+            if (!empty($response)) {
+
+                $result["blacklists"][$dns] = $response;
+
+            }
+
+        }
+        
+        return $result;
+
+    }
+    
+}
---
+++ example.php
@@ -0,0 +1,11 @@
+<?php
+
+use dastanaron\SpamChecker;
+
+$checker = new SpamChecker('blacklist.txt', 5);
+
+// Example clean address
+var_dump($checker->check("mail.ru"));
+
+// Example spam address
+var_dump($checker->check("182.244.194.17"));
